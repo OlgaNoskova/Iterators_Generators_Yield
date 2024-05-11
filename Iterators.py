3faf381eac25a5c8264e7280b0371ed9640eb275
Olga_Noskova <perhonen_87@mail.ru>
2024-05-11 10:34:13 +0300

    Initial commit

---
+++ Iterators.py
@@ -0,0 +1,44 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+
+    def __iter__(self):
+        self.cursor = -1
+        self.index = 0
+        return self
+
+    def __next__(self):
+        self.cursor += 1
+        self.item_list = self.list_of_list[self.index]
+        if self.cursor >= len(self.item_list):
+            self.cursor = 0
+            self.index += 1
+            if self.index == len(self.item_list):
+                raise StopIteration
+            self.item_list = self.list_of_list[self.index]
+        item = self.item_list[self.cursor]
+        return item
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
